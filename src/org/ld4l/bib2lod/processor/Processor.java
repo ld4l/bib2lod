e6d31dc11e3e8a6e5a97b8cfb94867cab219179f
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-07-29 13:19:54 -0400

    Add class and package structure

---
+++ src/org/ld4l/bib2lod/processor/Processor.java
@@ -0,0 +1,12 @@
+package org.ld4l.bib2lod.processor;
+
+public class Processor {
+
+    protected String localNamespace;
+    
+    public Processor(String localNamespace) {
+        
+        this.localNamespace = localNamespace;
+    }
+    
+}
---
+++ src/org/ld4l/bib2lod/processor/deduper/Deduper.java
@@ -0,0 +1,13 @@
+package org.ld4l.bib2lod.processor.deduper;
+
+import org.ld4l.bib2lod.processor.Processor;
+
+
+public class Deduper extends Processor {
+
+    public Deduper(String localNamespace) {
+        super(localNamespace);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
