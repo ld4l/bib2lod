99099734379e5549aae6956a24de19118cfc032f
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-07-29 15:10:20 -0400

    Add constructors

--- src/org/ld4l/bib2lod/processor/Processor.java
+++ src/org/ld4l/bib2lod/processor/Processor.java
@@ -1,12 +1,29 @@
 package org.ld4l.bib2lod.processor;
 
+import java.io.File;
+
 public class Processor {
 
     protected String localNamespace;
+    protected String rdfFormat;
     
-    public Processor(String localNamespace) {
+    protected File inputDir;
+    protected File outputDir;
+    protected File logDir;
+    
+    public Processor(String localNamespace, String rdfFormat, File inputDir, 
+            File outputDir, File logDir) {
         
         this.localNamespace = localNamespace;
+        this.rdfFormat = rdfFormat;
+        
+        this.inputDir = inputDir;
+        this.logDir = logDir;
+        this.outputDir = outputDir;
+    }
+    
+    public void process(String[] actions) {
+        System.out.println("Done processing!");
     }
     
 }
--- src/org/ld4l/bib2lod/processor/deduper/Deduper.java
+++ src/org/ld4l/bib2lod/processor/deduper/Deduper.java
@@ -1,12 +1,15 @@
 package org.ld4l.bib2lod.processor.deduper;
 
+import java.io.File;
+
 import org.ld4l.bib2lod.processor.Processor;
 
 
 public class Deduper extends Processor {
 
-    public Deduper(String localNamespace) {
-        super(localNamespace);
+    public Deduper(String localNamespace, String rdfFormat, File inputDir, 
+            File outputDir, File logDir) {
+        super(localNamespace, localNamespace, inputDir, inputDir, inputDir);
         // TODO Auto-generated constructor stub
     }
     
