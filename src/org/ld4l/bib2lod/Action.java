cbb58fc244633b874745b3de3f38aec9e096f1b6
Rebecca Younes <rjy7@lib-rjy7.local>
2015-08-18 11:15:46 -0400

    Add rank to Action enum

--- src/org/ld4l/bib2lod/Action.java
+++ src/org/ld4l/bib2lod/Action.java
@@ -7,28 +7,47 @@ import java.util.Map;
 
 public enum Action {
 
-    DEDUPE ("dedupe");
+    // GET_MARC ("get_marc", 1),
+    // MARC2MARCXML ("marc2marcxml", 2),
+    // Clean up MARCXML records: correct known errors, enhance with ??
+    // PREPROCESS_MARCXML ("preprocess", 3),
+    // MARCXML2BIBFRAME ("marcxml2bibframe", 4),
+    DEDUPE_BIBFRAME_URIS ("dedupe", 5);
+    // BIBFRAME2LD4L ("bibframe2ld4l, 6");
+    
+    private final String action;    
+    // Use for ordering actions
+    private final Integer rank;
 
-    private final String action;
-
-    Action(String action) {
+    // TODO Can we make this an int rather than an Integer
+    Action(String action, Integer rank) {
         this.action = action;
+        this.rank = rank;
     }
     
     public String action() {
         return this.action;
     }
     
+    public Integer rank() {
+        return this.rank;
+    }
+    
     private static final Map<String, Action> lookup = 
             new HashMap<String, Action>();
+
+    private static final Map<Integer, Action> byRank = 
+            new HashMap<Integer, Action>();
     
     private static List<String> validActions = 
             new ArrayList<String>();
-    
+
     static {
-        for(Action actionValue : Action.values()) {
+        for (Action actionValue : Action.values()) {
             String action = actionValue.action();
             lookup.put(action, actionValue);
+            Integer actionRank = actionValue.rank();
+            byRank.put(actionRank, actionValue);
             validActions.add(action);
         }
     }
@@ -37,8 +56,21 @@ public enum Action {
         return lookup.get(action); 
     }
     
+    public static Action getByRank(Integer rank) {
+        return byRank.get(rank);
+    }
+    
     public static List<String> validActions() {
         return validActions;
+    }
+    
+
+    
+
+    
+    public static Action getByRank(int rank) {
+        return byRank.get(rank);
+        
     }
     
 }
