c1088535d6f1532b9bebc241716fac34bb49bab3
Rebecca Younes <rjy7@lib-rjy7.local>
2015-08-18 11:44:26 -0400

    Add some enum Ontology values

--- src/org/ld4l/bib2lod/Ontology.java
+++ src/org/ld4l/bib2lod/Ontology.java
@@ -2,9 +2,12 @@ package org.ld4l.bib2lod;
 
 public enum Ontology {
 
+    BIBFRAME ("http://bibframe.org/vocab/"),
     BIBFRAME_LD4L ("http://bibframe.ld4l.org/ontology/"),
     DCTERMS ("http://purl.org/dc/terms/"),
-    FOAF ("http://http://xmlns.com/foaf/0.1/");
+    FOAF ("http://http://xmlns.com/foaf/0.1/"),
+    MADSRDF ("http://www.loc.gov/mads/rdf/v1#"), // check if needed
+    RELATORS ("http://id.loc.gov/vocabulary/relators/"); // check if needed
     
     private final String namespace;
     
