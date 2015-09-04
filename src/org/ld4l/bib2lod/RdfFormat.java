e8c72251bfaae343dd24b07ab3de8505a8951461
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-09-04 12:08:59 -0400

    Restored previous code; not yet using

--- src/org/ld4l/bib2lod/RdfFormat.java
+++ src/org/ld4l/bib2lod/RdfFormat.java
@@ -1,5 +1,10 @@
 package org.ld4l.bib2lod;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.jena.riot.RDFFormat;
 
 public enum RdfFormat {
@@ -28,5 +33,30 @@ public enum RdfFormat {
     public String extension() {
         return this.extension;
     }
-    
+
+    public String fullExtension() {
+        return "." + this.extension;
+    }
+ 
+    private static final Map<String, RdfFormat> lookupByLabel =
+            new HashMap<String, RdfFormat>();
+   
+    private static List<RdfFormat> validFormats =
+            new ArrayList<RdfFormat>();
+   
+    static {
+        for (RdfFormat rf : RdfFormat.values()) {
+            String label = rf.label();
+            lookupByLabel.put(label, rf);
+            validFormats.add(rf);
+        }
+    }
+
+    public static RdfFormat get(String label) {
+        return lookupByLabel.get(label);
+    }
+   
+    public static List<RdfFormat> validFormats() {
+        return validFormats;
+    }
 }
