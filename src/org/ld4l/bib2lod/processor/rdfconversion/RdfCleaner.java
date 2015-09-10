a444747ec9c8852b5ac663826238eb9811b77183
Rebecca Younes <rjy7@dhcp-legacy-2666.redrover.cornell.edu>
2015-09-10 16:05:18 -0400

    Encode URI method

--- src/org/ld4l/bib2lod/processor/rdfconversion/RdfCleaner.java
+++ src/org/ld4l/bib2lod/processor/rdfconversion/RdfCleaner.java
@@ -6,6 +6,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,8 +23,8 @@ import org.ld4l.bib2lod.processor.Processor;
 public class RdfCleaner extends Processor {
 
     private static final Logger LOGGER = LogManager.getLogger(RdfCleaner.class);
-    private static final Pattern URI_PATTERN = Pattern.compile("http://[^\"><]+");
-    
+    private static final Pattern URI_PATTERN = 
+            Pattern.compile("(?<=<)http://[^>]+(?=>)|(?<!<)http://[^\"><]+(?!>)");
     public RdfCleaner(String localNamespace, String inputDir,
             String mainOutputDir) {
         super(localNamespace, inputDir, mainOutputDir);
@@ -59,30 +63,46 @@ public class RdfCleaner extends Processor {
     }
 
     private String processLine(String line) {
-        // LOGGER.debug("original: " + line);
-        line = fixUris(line);
-        // LOGGER.debug("new: " + line);
+        line = encodeUris(line);
+        // Any other operations go here
         return line;
     
     }
     
-    private String fixUris(String line) {
-
-        Matcher m = URI_PATTERN.matcher(line);
-        StringBuffer result = new StringBuffer();
-        while (m.find()) {
-            /* Could do a uri encoding here. Then the new string will be a
-             * different length from the original. Use technique described in
-             * Matching Regular Expressions p.383.
-             */
-            LOGGER.debug(m.group());
-            String replacement = m.group().replaceAll(" ", "_");
-            m.appendReplacement(result, replacement);
+    private String encodeUris(String line) {
+        // LOGGER.debug("Original line: " + line);     
+        StringBuilder text = new StringBuilder(line);       
+        Matcher m = URI_PATTERN.matcher(text);
+        int matchPointer = 0;
+        while (m.find(matchPointer)) {
+            matchPointer = m.end();
+            try {
+                String match = m.group();
+                URL url = new URL(match);
+                // Only the multi-argument URI constructor encodes illegal
+                // characters, so use URL methods to break up the string into
+                // components to feed to the URI constructor.
+                String uri = new URI(url.getProtocol(), url.getUserInfo(), 
+                        url.getHost(), url.getPort(), url.getPath(), 
+                        url.getQuery(), url.getRef()).toString();
+                uri = uri.replace("&#34;", "%22");
+                if (! uri.equals(m.group().toString())) {
+                    text.replace(m.start(), m.end(), uri);
+                    LOGGER.debug("Encoding illegal characters:");
+                    LOGGER.debug(match);
+                    LOGGER.debug("uri = " + uri);
+                    matchPointer += uri.length() - match.length();
+                }
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
-        m.appendTail(result);
-        return result.toString();
+        // LOGGER.debug("New line: " + text.toString());
+        return text.toString();
     }
-
-
 
 }
