4d2dbd578c48b0de49b7a490474c8310b4f150e3
Rebecca Younes <rjy7@dhcp-legacy-262.redrover.cornell.edu>
2015-09-16 13:48:47 -0400

    Testing how Jena handles duplicate triples

--- src/org/ld4l/bib2lod/JenaTest.java
+++ src/org/ld4l/bib2lod/JenaTest.java
@@ -3,6 +3,9 @@ package org.ld4l.bib2lod;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -18,23 +21,28 @@ public class JenaTest {
     public static void main(String[] args) {
         
         /**
-         * Test whether reading into a Jena model removes duplicate triples.
-         * Result: yes.
+         * Test whether reading in and/or writing out a Jena model removes 
+         * duplicate triples.
+         * Result: duplicates removed when reading the file into a model.
          */
         String infile = "testinput/jenatest/test.nt";
-        Model model = ModelFactory.createDefaultModel() ; 
-        model.read(infile);
-        
-        File outfile = new File("output/jenatest/test.nt");
-        FileOutputStream outStream;
+        String outfile = "output/jenatest/test.nt";
         try {
+            long incount = Files.newBufferedReader(Paths.get(infile)).lines().count();
+            LOGGER.debug("Input file size: " + incount);
+            Model model = ModelFactory.createDefaultModel() ; 
+            model.read(infile);
+            LOGGER.debug("Triples in model: " + model.size());
+            FileOutputStream outStream;
             outStream = new FileOutputStream(outfile);
             RDFDataMgr.write(outStream, model, RDFFormat.NTRIPLES);
-        } catch (FileNotFoundException e) {
+            long outcount = Files.newBufferedReader(Paths.get(outfile)).lines().count();
+            LOGGER.debug("Input file size: " + outcount);
+        } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }   
-        
+        }
+
         LOGGER.info("Done!");
 
     }
