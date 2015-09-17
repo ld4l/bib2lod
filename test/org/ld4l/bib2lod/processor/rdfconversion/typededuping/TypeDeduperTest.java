e3ddbacd837f10942da3a086a68a709cd99cc1ba
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2015-09-17 15:21:45 -0400

    Add TypeDeduperTest class

---
+++ test/org/ld4l/bib2lod/processor/rdfconversion/typededuping/TypeDeduperTest.java
@@ -0,0 +1,64 @@
+package org.ld4l.bib2lod.processor.rdfconversion.typededuping;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TypeDeduperTest {
+    
+    private BfPersonDeduper deduper = new BfPersonDeduper();
+
+    @Test
+    public void testNormalizeAgentName1() {
+        
+        String normalizedName = "twain,_mark_1835_1910";        
+        String name = "Twain, Mark, 1835-1910";
+        name = deduper.normalizeAgentName(name);
+        Assert.assertEquals(name, normalizedName);
+        
+    }
+
+    @Test
+    public void testNormalizeAgentName2() {
+
+        String normalizedName = "twain,_mark_1835_1910";        
+        String name = "Twain, Mark, 1835-1910.";
+        name = deduper.normalizeAgentName(name);
+        Assert.assertEquals(name, normalizedName);
+        
+    }
+    
+    @Test
+    public void testNormalizeAgentName3() {
+
+      String normalizedName = "gordon,_burgess_l_burgess_lee_1892";      
+      String name = "Gordon, Burgess L. (Burgess Lee), 1892-";
+      name = deduper.normalizeAgentName(name);
+      Assert.assertEquals(name, normalizedName);
+      
+    }
+
+    
+    @Test
+    public void testNormalizeAgentName4() {
+
+        String normalizedName = "railton,_stephen_1948";
+        String name = "Railton, Stephen, 1948-"; 
+        name = deduper.normalizeAgentName(name);
+        Assert.assertEquals(name, normalizedName);        
+    }
+    
+    @Test
+    public void testNormalizeAgentName5() {
+
+
+        
+    }
+    
+    @Test
+    public void testNormalizeAgentName6() {
+
+
+        
+    }
+    
+}
