cb8ce9199aef9b385ea05ea2fae88b6d28acaa2f
Rebecca Younes <rjy7@lib-rjy7.local>
2015-10-05 17:44:28 -0400

    Commented out modifications
    Working on Instance Identifiers and Titles - commented out for now

--- src/org/ld4l/bib2lod/rdfconversion/typededuping/BfInstanceDeduper.java
+++ src/org/ld4l/bib2lod/rdfconversion/typededuping/BfInstanceDeduper.java
@@ -1,8 +1,6 @@
 package org.ld4l.bib2lod.rdfconversion.typededuping;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.jena.query.Query;
@@ -11,6 +9,7 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
@@ -31,6 +30,7 @@ public class BfInstanceDeduper extends TypeDeduper {
     private static final Logger LOGGER =          
             LogManager.getLogger(BfInstanceDeduper.class);
     
+    
     private static final String WORLDCAT_NS = "http://www.worldcat.org/oclc/";
 
     @Override
@@ -39,14 +39,23 @@ public class BfInstanceDeduper extends TypeDeduper {
         LOGGER.debug("Deduping type " + type.toString());
         
         // Maps local URIs in the Bibframe RDF to deduped URIs (single, unique
-        // URI per Instance). This map will be used to replace duplicate URIs
-        // for the same Instance with a single, unique URI.
+        // URI per Instance, Identifier, and Title). This map will be used to 
+        // replace duplicate URIs for the same entities with a single, unique 
+        // URI.
         Map<String, String> uniqueUris = new HashMap<String, String>();
         
         // Maps keys for Instance identity matching to the unique Topic URIs.
         // Note that this is also a worldcat URI to local unique URI mapping.
         Map<String, String> uniqueInstances = new HashMap<String, String>();
-        
+
+//        // Maps keys for Identifier identity matching to the unique Identifier
+//        // URIs.
+//        Map<String, String> uniqueIdentifiers = new HashMap<String, String>();
+//        
+//        // Maps keys for Title identity matching to the unique Title
+//        // URIs.
+//        Map<String, String> uniqueTitles = new HashMap<String, String>();
+
         // Execute the query
         Query query = getQuery();        
         QueryExecution qexec = QueryExecutionFactory.create(query, model);
@@ -63,7 +72,14 @@ public class BfInstanceDeduper extends TypeDeduper {
                 LOGGER.debug("No key (worldcat id) for " + instanceUri 
                         + "; can't dedupe");
                 continue;
-            }      
+            }   
+            
+//            Resource nonWorldCatId = soln.getResource("nonWorldCatId");
+//            if (nonWorldCatId != null) {                    
+//                String idUri = nonWorldCatId.getURI();
+//                String idKey = getIdentifierKey(soln);
+//            }            
+            // SAME FOR TITLE
 
             // TODO *** We may want to substitute the worldcat uri for the local
             // one - pending response from Steven. If not, we need to add a
@@ -81,6 +97,7 @@ public class BfInstanceDeduper extends TypeDeduper {
                 // This local Instance URI will be replaced by the unique 
                 // Instance URI throughout the data. 
                 uniqueUris.put(instanceUri, uniqueInstanceUri);
+
 
             } else {
                 // We haven't seen this Instance before
@@ -122,6 +139,7 @@ public class BfInstanceDeduper extends TypeDeduper {
         String queryString = 
                 "PREFIX fn: <http://www.w3.org/2005/xpath-functions#>  " 
                 + "SELECT ?instance ?worldcatId "
+                // + "?nonWorldCatId ?nonWorldCatIdScheme ?nonWorldCatIdValue "
                 + "WHERE { "
                 + "?instance a " + OntType.BF_INSTANCE.sparqlUri() + " . "
                 + "OPTIONAL { ?instance "
@@ -129,8 +147,26 @@ public class BfInstanceDeduper extends TypeDeduper {
                 + "?worldcatId .  "
                 + "FILTER ( fn:starts-with(str(?worldcatId), "
                 + "\"" + WORLDCAT_NS + "\"  ) ) } "
+                // + "OPTIONAL { ?instance "
+                // + OntProperty.BF_SYSTEM_NUMBER.sparqlUri() + " "
+                // + "?nonWorldCatId . "
+                // + "?nonWorldCatId " 
+                // + OntProperty.BF_IDENTIFIER_SCHEME.sparqlUri() + " "
+                // + "?nonWorldCatIdScheme ; " 
+                // + OntProperty.BF_IDENTIFIER_VALUE.sparqlUri() + " "
+                // + " ?nonWorldCatIdValue ; "
+                // + "FILTER ( ! fn:starts-with(str(?nonWorldCatId), "
+                // + "\"" + WORLDCAT_NS + "\"  ) ) } "
                 + "}";
 
+        
+
+//        + "?s1 ?p1 ?o1 . "
+//        + "?s1 a ?type . "
+//        + "?o1 ?p2 ?o2 . "
+//        + "?o1 a " + OntType.BF_TITLE.sparqlUri()
+        
+        
         LOGGER.debug("QUERY: " + queryString);
         return QueryFactory.create(queryString);
 
@@ -145,5 +181,19 @@ public class BfInstanceDeduper extends TypeDeduper {
         }
         return null;
     }
+    
+//    private String getIdentifierKey(QuerySolution soln) {
+//        
+//        // Dedupe Instance Identifiers on scheme and value
+//        Resource id = soln.getResource("id");
+//        if (id != null) {
+//            Resource scheme = soln.getResource("idScheme");
+//            String schemeUri = scheme == null ? "" : scheme.getURI();
+//            Literal valueLiteral = soln.getLiteral("idValue");
+//            String value = valueLiteral == null ? "" : valueLiteral.getLexicalForm();
+//            return schemeUri + value;
+//        }
+//        return null;
+//    }
 
 }
