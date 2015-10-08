cb6cf9a07d1d4436def2c6b3e7bcfb6429b9212e
Rebecca Younes <rjy7@dhcp-legacy-576.redrover.cornell.edu>
2015-10-08 12:03:10 -0400

    Refactor: create Processor superclass

---
+++ src/org/ld4l/bib2lod/Processor.java
@@ -0,0 +1,86 @@
+package org.ld4l.bib2lod;
+
+import static java.nio.file.StandardCopyOption.COPY_ATTRIBUTES;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class Processor {
+    
+    private static final Logger LOGGER = LogManager.getLogger(Processor.class);  
+        
+    private String outputDir;
+    private final String mainOutputDir;
+    protected final String inputDir;
+
+
+    /**
+     * Constructor for processes that don't use the loaded OntModel(s).
+     * @param localNamespace
+     * @param inputDir
+     * @param mainOutputDir
+     */
+    public Processor(String inputDir, String mainOutputDir) {
+            
+        LOGGER.info("In constructor for " + this.getClass().toString());            
+        this.inputDir = inputDir;
+        this.mainOutputDir = mainOutputDir;
+        this.outputDir = createOutputDir();
+               
+    }
+
+    public abstract String process();
+
+    protected String getOutputDir() {
+        return outputDir;
+    }
+
+    /**
+     * Create a subdirectory of main output directory for the output of this
+     * process.
+     * @return - the full output directory path
+     */
+    private String createOutputDir() {
+        
+        String outputDir = null;
+        String outputSubdir = 
+                StringUtils.substringAfterLast(this.getClass().getName(), "."); 
+        try {
+            outputDir = Files.createDirectory(Paths.get(mainOutputDir, 
+                    outputSubdir)).toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // TODO Abort program here
+        }
+        
+        return outputDir;
+    }
+    
+
+    protected String stubProcess() {
+        copyFiles(inputDir, outputDir);
+        return outputDir;
+    }
+    
+    protected void copyFiles(String inputDir, String outputDir) { 
+        
+        for ( File file : new File(inputDir).listFiles() ) {
+            Path source = file.toPath();
+            String sourceFileName = file.getName();
+            Path target = new File(outputDir, sourceFileName).toPath();
+            try {
+                Files.copy(source, target, COPY_ATTRIBUTES);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/org/ld4l/bib2lod/rdfconversion/RdfProcessor.java
+++ src/org/ld4l/bib2lod/rdfconversion/RdfProcessor.java
@@ -1,16 +1,9 @@
 package org.ld4l.bib2lod.rdfconversion;
 
-import static java.nio.file.StandardCopyOption.COPY_ATTRIBUTES;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -21,9 +14,10 @@ import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.ld4l.bib2lod.Processor;
 
 
-public abstract class RdfProcessor {
+public abstract class RdfProcessor extends Processor {
 
     private static final Logger LOGGER = LogManager.getLogger(RdfProcessor.class);   
     
@@ -32,15 +26,12 @@ public abstract class RdfProcessor {
      * repeated (not sure whether json and ttl would work); (2) The links to 
      * blank nodes are lost, since no identifier is assigned to them. (2) is no
      * longer relevant once BnodeConverter has applied.  
-       private static final RdfFormat DEFAULT_RDF_OUTPUT_FORMAT = 
+       private static final RdfFormat DEFAULT_OUTPUT_FORMAT = 
             RdfFormat.NTRIPLES;
      */
-    private static final RdfFormat RDF_OUTPUT_FORMAT = RdfFormat.NTRIPLES;
+    protected static final RdfFormat OUTPUT_FORMAT = RdfFormat.NTRIPLES;
             
-    private String outputDir;
     protected final String localNamespace;    
-    protected final String inputDir;
-    private final String mainOutputDir;
     protected OntModel bfOntModelInf; 
 
        
@@ -59,32 +50,18 @@ public abstract class RdfProcessor {
      */
     public RdfProcessor(String localNamespace, String inputDir, 
             String mainOutputDir) {
-
+        super(inputDir, mainOutputDir);
         LOGGER.info("In constructor for " + this.getClass().toString());        
-        this.localNamespace = localNamespace;       
-        this.inputDir = inputDir;
-        this.mainOutputDir = mainOutputDir;
-        this.outputDir = createOutputDir();
-               
+        this.localNamespace = localNamespace;                      
     }
     
     public abstract String process();
     
-    protected String getOutputDir() {
-        return outputDir;
+    protected String getOutputFilename(String basename) {
+        //return basename + "." +  getRdfOutputFormat().extension(); 
+        return basename + getRdfOutputFormat().fullExtension();
     }
 
-    protected Model readModelFromFile(File file) {
-        return readModelFromFile(file.toString());
-    }
-    
-    protected Model readModelFromFile(String filename) {
-        //return RDFDataMgr.loadModel(filename);
-        Model model = ModelFactory.createDefaultModel() ; 
-        model.read(filename);
-        return model;
-    }
-    
     protected RdfFormat getRdfOutputFormat() {
         /* Currently allowing only ntriples output format: Some processors MUST 
          * output ntriples, for two reasons: (1) append to file doesn't produce
@@ -100,30 +77,20 @@ public abstract class RdfProcessor {
          * 2. Commandline option
          * 3. Same format as input
          */
-        return RDF_OUTPUT_FORMAT;
+        return OUTPUT_FORMAT;
     }
     
-    /**
-     * Create a subdirectory of main output directory for the output of this
-     * process.
-     * @return - the full output directory path
-     */
-    private String createOutputDir() {
-        
-        String outputDir = null;
-        String outputSubdir = 
-                StringUtils.substringAfterLast(this.getClass().getName(), "."); 
-        try {
-            outputDir = Files.createDirectory(Paths.get(mainOutputDir, 
-                    outputSubdir)).toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-            // TODO Abort program here
-        }
-        
-        return outputDir;
+    protected Model readModelFromFile(File file) {
+        return readModelFromFile(file.toString());
     }
     
+    protected Model readModelFromFile(String filename) {
+        //return RDFDataMgr.loadModel(filename);
+        Model model = ModelFactory.createDefaultModel() ; 
+        model.read(filename);
+        return model;
+    }
+
     /** 
      * Debugging method: Output all statements in a model
      */
@@ -174,7 +141,7 @@ public abstract class RdfProcessor {
     }
     
     private void writeModelToFile(Model model, String basename, boolean append) {
-        File outputFile = new File(outputDir, getOutputFilename(basename));
+        File outputFile = new File(getOutputDir(), getOutputFilename(basename));
         writeModelToFile(model, outputFile, append);
     }
     
@@ -189,30 +156,5 @@ public abstract class RdfProcessor {
             e.printStackTrace();
         }        
     }
-    
-    protected String getOutputFilename(String basename) {
-        //return basename + "." +  getRdfOutputFormat().extension(); 
-        return basename + getRdfOutputFormat().fullExtension();
-    }
-    
-    protected String stubProcess() {
-        copyFiles(inputDir, outputDir);
-        return outputDir;
-    }
-    
-    protected void copyFiles(String inputDir, String outputDir) { 
-        
-        for ( File file : new File(inputDir).listFiles() ) {
-            Path source = file.toPath();
-            String sourceFileName = file.getName();
-            Path target = new File(outputDir, sourceFileName).toPath();
-            try {
-                Files.copy(source, target, COPY_ATTRIBUTES);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
- 
+
 }
