2bb04f89bfde20f2f6af090a5a484a4d3b1f8642
Rebecca Younes <rjy7@dhcp-legacy-49.redrover.cornell.edu>
2015-11-04 10:24:56 -0500

    Clean up imports

--- src/org/ld4l/bib2lod/rdfconversion/bibframeconversion/BfHeldItemConverter.java
+++ src/org/ld4l/bib2lod/rdfconversion/bibframeconversion/BfHeldItemConverter.java
@@ -12,7 +12,6 @@ import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.vocabulary.RDF;
-import org.apache.jena.vocabulary.RDFS;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.ld4l.bib2lod.rdfconversion.BfProperty;
