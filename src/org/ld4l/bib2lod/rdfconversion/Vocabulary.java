1f32972d6901da28d5ba8eb7b2cc8e696356e252
Rebecca Younes <rjy7@dhcp-legacy-503.redrover.cornell.edu>
2015-11-09 13:53:56 -0500

    Add a Vocabulary enum value

--- src/org/ld4l/bib2lod/rdfconversion/Vocabulary.java
+++ src/org/ld4l/bib2lod/rdfconversion/Vocabulary.java
@@ -3,9 +3,11 @@ package org.ld4l.bib2lod.rdfconversion;
 public enum Vocabulary {
 
     FAST("http://id.worldcat.org/fast/", "fast"),
+    LANGUAGE("http://id.loc.gov/vocabulary/languages/", "languages"),
     WORLDCAT("http://www.worldcat.org/oclc/", "worldcat"),
     
-    // Invented vocabularies for demonstration purposes 
+    // Invented vocabularies to create demonstration URIs. Should be replaced
+    // with actual shelf mark vocabulary namespaces.
     DDC("http://id.loc.gov/ddc/", "ddc"),
     LCC("http://id.loc.gov/lcc/", "lcc"),
     NLM("http://id.loc.gov/nlm/", "nlm"),
