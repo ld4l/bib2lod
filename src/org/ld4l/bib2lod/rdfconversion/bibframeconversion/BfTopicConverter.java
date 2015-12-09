3fa91f6a27375d305485d8d2827c7f828c9fd7c3
Rebecca Younes <rjy7@dhcp-legacy-801.redrover.cornell.edu>
2015-12-09 13:00:05 -0500

    Convert Topic bf:label to skos:prefLabel

--- src/org/ld4l/bib2lod/rdfconversion/bibframeconversion/BfTopicConverter.java
+++ src/org/ld4l/bib2lod/rdfconversion/bibframeconversion/BfTopicConverter.java
@@ -1,12 +1,15 @@
 package org.ld4l.bib2lod.rdfconversion.bibframeconversion;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.ld4l.bib2lod.rdfconversion.BfProperty;
 import org.ld4l.bib2lod.rdfconversion.BfType;
+import org.ld4l.bib2lod.rdfconversion.Ld4lProperty;
 import org.ld4l.bib2lod.rdfconversion.Vocabulary;
 
 public class BfTopicConverter extends BfAuthorityConverter {
@@ -14,11 +17,17 @@ public class BfTopicConverter extends BfAuthorityConverter {
     private static final Logger LOGGER = 
             LogManager.getLogger(BfTopicConverter.class);
 
-    private static List<BfProperty> PROPS_TO_REMOVE = 
+    private static List<BfProperty> FAST_PROPERTIES_TO_RETRACT = 
             new ArrayList<BfProperty>();
     static {
-        PROPS_TO_REMOVE.add(BfProperty.BF_HAS_AUTHORITY);
-        PROPS_TO_REMOVE.add(BfProperty.BF_SYSTEM_NUMBER);
+        FAST_PROPERTIES_TO_RETRACT.add(BfProperty.BF_HAS_AUTHORITY);
+        FAST_PROPERTIES_TO_RETRACT.add(BfProperty.BF_SYSTEM_NUMBER);
+    }
+    
+    private static final Map<BfProperty, Ld4lProperty> PROPERTY_MAP = 
+            new HashMap<BfProperty, Ld4lProperty>();
+    static {
+        PROPERTY_MAP.put(BfProperty.BF_LABEL, Ld4lProperty.PREFERRED_LABEL);
     }
 
     public BfTopicConverter(BfType bfType, String localNamespace) {
@@ -39,8 +48,12 @@ public class BfTopicConverter extends BfAuthorityConverter {
     private void convertFastTopic() {
         String namespace = subject.getNameSpace();
         if (namespace.equals(Vocabulary.FAST.uri())) {
-            removeResources(PROPS_TO_REMOVE);  
+            removeResources(FAST_PROPERTIES_TO_RETRACT);  
         }
     }
 
+    @Override
+    protected Map<BfProperty, Ld4lProperty> getBfPropertyMap() {
+        return PROPERTY_MAP;
+    }
 }
