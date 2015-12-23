806ae3b2b2c03c7f142649bce2c656ed09d0e5ca
Rebecca Younes <rjy7@dhcp-legacy-2083.redrover.cornell.edu>
2015-12-23 11:35:18 -0500

    Fix another namespace value

--- src/org/ld4l/bib2lod/rdfconversion/OntNamespace.java
+++ src/org/ld4l/bib2lod/rdfconversion/OntNamespace.java
@@ -20,7 +20,7 @@ public enum OntNamespace {
     OWL("http://www.w3.org/2002/07/owl#", "owl"),
     PROV("http://www.w3.org/ns/prov#", "prov"),
     RDF("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "rdf"),
-    RDFS("http://http://www.w3.org/2000/01/rdf-schema#", "rdfs"),
+    RDFS("http://www.w3.org/2000/01/rdf-schema#", "rdfs"),
     RELATORS("http://id.loc.gov/vocabulary/relators/", "marcrel"), 
     SCHEMA("http://schema.org/", "schema"),
     SKOS("http://www.w3.org/2004/02/skos/core#", "skos"),
