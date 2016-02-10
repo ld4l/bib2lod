eb5d34757fc3fdafb43966338f024a4c3d25d73b
Rebecca Younes <rjy7@lib-c02mjedqfd57.library.cornell.edu>
2016-02-10 15:14:48 -0500

    Continue work on unique URI generation
    Map local names to unique local names so unique values do not have to be recomputed.

--- src/org/ld4l/bib2lod/rdfconversion/UriGenerator.java
+++ src/org/ld4l/bib2lod/rdfconversion/UriGenerator.java
@@ -3,9 +3,15 @@ package org.ld4l.bib2lod.rdfconversion;
 import java.io.File;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.query.ParameterizedSparqlString;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.RDFNode;
@@ -23,7 +29,14 @@ public class UriGenerator extends RdfProcessor {
     
     private static final Logger LOGGER = 
             LogManager.getLogger(UriGenerator.class);
-
+    
+    private static ParameterizedSparqlString pss = 
+            new ParameterizedSparqlString(
+                    "CONSTRUCT { ?s ?p ?o } WHERE { " 
+                    + "?s ?p ?o "
+                    + "FILTER (?s = ?uri || ?o = ?uri)"
+                    + " }");
+    
     public UriGenerator(OntModel bfOntModelInf, String localNamespace, 
             String inputDir, String mainOutputDir) {
         super(bfOntModelInf, localNamespace, inputDir, mainOutputDir);
@@ -105,45 +118,58 @@ public class UriGenerator extends RdfProcessor {
     
     private Model processInputFile(File inputFile) {
         
-        Model inputModel = readModelFromFile(inputFile);
-        
+        Model inputModel = readModelFromFile(inputFile);    
         Model outputModel = ModelFactory.createDefaultModel();
+        Map<String, String> uniqueLocalNames = new HashMap<String, String>();
 
         StmtIterator statements = inputModel.listStatements();
         while (statements.hasNext()) {
             Statement statement = statements.next();
-            Statement newStatement = generateUniqueUris(statement);                    
+            Statement newStatement = generateUniqueUris(
+                    statement, uniqueLocalNames, outputModel);                    
             outputModel.add(newStatement);
         }  
         
         return outputModel;
     }
 
-    private Statement generateUniqueUris(Statement statement) {
+    private Statement generateUniqueUris(Statement statement, 
+            Map<String, String> uniqueLocalNames, Model outputModel) {
             
         Resource subject = statement.getSubject();
-        Resource newSubject = getUniqueResource(subject);
+        Resource newSubject = getUniqueResource(
+                subject, uniqueLocalNames, outputModel);
         
         RDFNode object = statement.getObject();
-        RDFNode newObject = object.isResource() ?
-                getUniqueResource(object.asResource()) :
-                    object;
-        
-        return ResourceFactory.createStatement(
+        RDFNode newObject = object.isLiteral() ? object :
+                getUniqueResource(
+                        object.asResource(), uniqueLocalNames, outputModel);
+     
+        // Model.createStatement() may reuse an existing statement rather than
+        // creating a new one.
+        return outputModel.createStatement(
                 newSubject, statement.getPredicate(), newObject);     
     }
 
-    private Resource getUniqueResource(Resource resource) {
+    private Resource getUniqueResource(Resource resource, 
+            Map<String, String> uniqueLocalNames, Model outputModel) {
             
-        String localname = resource.isAnon() ? 
-                getLocalNameFromAnonNode(resource) :
-                    getUniqueLocalNameForResource(resource);
+        String localName;
+        
+        if (resource.isAnon()) {
+            localName = getLocalNameForAnonNode(resource);
+            
+        } else if (! resource.getNameSpace().equals(localNamespace)) {
+            return resource;
 
-        // Does it matter whether we create the resource from a ResourceFactory
-        // rather than using outputModel.createResource()? Will the resource 
-        // still  belong to the output model once the statement is added? Does 
-        // it matter, since we're just outputting text in the end?
-        return ResourceFactory.createResource(localNamespace + localname);
+        } else {
+            localName = getUniqueLocalNameForResource(
+                    resource, uniqueLocalNames);
+        }
+        
+        // Model.createResource() may reuse an existing resource rather than
+        // creating a new one.
+        return outputModel.createResource(localNamespace + localName);
 
     }
 
@@ -152,44 +178,85 @@ public class UriGenerator extends RdfProcessor {
      * may duplicate bnode ids across files when the entities should be 
      * distinct. Assigning fixed URIs solves this problem.
      */
-    private String getLocalNameFromAnonNode(Resource resource) {
-        return resource.asNode().getBlankNodeId().toString();
+    private String getLocalNameForAnonNode(Resource bnode) {
+        return bnode.asNode().getBlankNodeId().toString();
     }
     
     /*
      * This local name serves to dedupe the same resource across records,
      * based on type-specific identifying data.
      */
-    private String getUniqueLocalNameForResource(Resource resource) {
-            
-        // TEMPORARY! 
-        return resource.getLocalName();
+    private String getUniqueLocalNameForResource(
+            Resource resource, Map<String, String> uniqueLocalNames) {
+                
+        String localName = resource.getLocalName();
         
-//        // The resource type determines the query to get the identifying data.
-//        Resource type = getResourceType(resource);
+        // If we've encountered this local name before, return the hashed value
+        // from the map.
+        if (uniqueLocalNames.containsKey(localName)) {
+            return uniqueLocalNames.get(localName);
+        }
+        
+        // Otherwise, compute a new hashed value.
+        
+        // First, get the submodel containing all statements in which this
+        // resource is either the subject or object.
+        Model resourceSubModel = getResourceSubModel(resource);
+          
+        // The resource type determines the query to get the identifying data.
+        Resource type = getResourceType(resource, resourceSubModel);
 //        
 //        // Get the identifying data and flatten to a string
 //        String key = getUniqueKey(resource, type);
 //        
-//        // Hash the string to get the local name
-//        String localname = getHashCode(key);
+//        // Hash the string to get the new local name
+//        String uniqueLocalName = getHashCode(key);
 //              
-//        return localname;
+      
+        // TEMPORARY!  
+        // Append "unique" just so we know this is working.
+        String uniqueLocalName = localName + "unique";
+        
+        uniqueLocalNames.put(localName, uniqueLocalName);
+        return uniqueLocalName;
     }
     
-    private Resource getResourceType(Resource resource) {
+    
+    /*
+     * Get the submodel of the input model consisting of statements in which 
+     * this resource is either the subject or object.
+     */
+    private Model getResourceSubModel(Resource resource) {
+        
+        Model inputModel = resource.getModel();
+
+        pss.setIri("uri", resource.getURI());
+        
+        Query query = pss.asQuery();
+        QueryExecution qexec = QueryExecutionFactory.create(
+                query, inputModel);
+        Model constructModel = qexec.execConstruct();
+        
+        return constructModel;
+    }
+    
+
+    private Resource getResourceType(Resource resource, Model model) {
         
         Resource type = resource.hasProperty(RDF.type) ?
-                getType(resource) :
-                    inferTypeFromPredicates(resource);
+                getType(resource, model) :
+                    inferTypeFromPredicates(resource, model);
+                
+        // TODO If the type had to be inferred, add it to the outputModel so 
+        // that it doesn't have to be re-inferred during Bibframe conversion.
         return type;
     }
         
-    private Resource getType(Resource resource) {
+    private Resource getType(Resource resource, Model model) {
         return null;
     }
 
-    private Resource inferTypeFromPredicates(Resource resource) {           
+    private Resource inferTypeFromPredicates(Resource resource, Model model) {           
         // TODO Auto-generated method stub
         return null;
     }
