5eebe27a616b8d4ddf74ffa6b1db2b5c7bf948db
Rebecca Younes <rjy7@lib-rjy7.local>
2016-02-29 15:01:57 -0500

    Remove BF authority-related props from meetings
    The Meeting does not have an Authority, since it is an event

--- src/org/ld4l/bib2lod/rdfconversion/bibframeconversion/BfMeetingConverter.java
+++ src/org/ld4l/bib2lod/rdfconversion/bibframeconversion/BfMeetingConverter.java
@@ -1,32 +1,61 @@
 package org.ld4l.bib2lod.rdfconversion.bibframeconversion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.jena.query.ParameterizedSparqlString;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QuerySolution;
-import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Property;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.ld4l.bib2lod.rdfconversion.BfProperty;
 import org.ld4l.bib2lod.rdfconversion.BfType;
 import org.ld4l.bib2lod.rdfconversion.Ld4lType;
-import org.ld4l.bib2lod.rdfconversion.RdfProcessor;
 
-public class BfMeetingConverter extends BfAuthorityConverter {
+public class BfMeetingConverter extends BfResourceConverter {
 
     private static final Logger LOGGER = 
             LogManager.getLogger(BfMeetingConverter.class);
 
-
+    private static List<BfProperty> propertiesToRetract = 
+            new ArrayList<BfProperty>();
+    static {
+        propertiesToRetract.add(BfProperty.BF_AUTHORITY_SOURCE);
+        propertiesToRetract.add(BfProperty.BF_AUTHORIZED_ACCESS_POINT);
+        propertiesToRetract.add(BfProperty.BF_HAS_AUTHORITY);
+    }
+    
+    private static ParameterizedSparqlString resourceSubModelPss = 
+            new ParameterizedSparqlString(
+                    "CONSTRUCT { ?resource ?p1 ?o1 . "
+                    + " ?o1 ?p2 ?o2 . "
+                    + " ?s ?p3 ?resource . " 
+                    + "} WHERE { { "  
+                    + "?resource ?p1 ?o1 . "
+                    + "OPTIONAL { ?o1 ?p2 ?o2 . } "
+                    + "} UNION { " 
+                    + "?s ?p3 ?resource . "
+                    + "} } ");
+    
     ParameterizedSparqlString askPss = new ParameterizedSparqlString(
             "ASK { "
             + "?meeting " + BfProperty.BF_HAS_AUTHORITY.sparqlUri() + " " 
             + "?madsAuth . "
             + "?madsAuth a " + BfType.MADSRDF_CONFERENCE_NAME.sparqlUri()
+            + "}");
+    
+    ParameterizedSparqlString selectPss = new ParameterizedSparqlString(
+            "SELECT ?idValue WHERE {"
+            + "?work + " + BfProperty.BF_SUBJECT.sparqlUri() + " "
+            + "?meeting . "
+            + "?meeting " + BfProperty.BF_SYSTEM_NUMBER.sparqlUri() + " "
+            + "?identifier . "
+            + "?identifier " + BfProperty.BF_IDENTIFIER_VALUE.sparqlUri() + " "
+            + "?idValue . "
             + "}");
 
     
@@ -34,25 +63,21 @@ public class BfMeetingConverter extends BfAuthorityConverter {
         super(localNamespace);
     }
     
+    @Override 
+    protected ParameterizedSparqlString getResourceSubModelPss() {
+        return resourceSubModelPss;
+    }
+    
     
     @Override
     protected Model convert() {
-        RdfProcessor.printModel(subject.getModel(), "Meeting construct model:");
         convertConferenceName();
         
-        /* 
-         * TODO ** What to do with Identifier, Authority, since we're changing
-         * Meeting from an Authority to an Event?
-         * Predicates of which Meetings are subjects:
-         * bf:authoritySource
-         * bf:authorizedAccessPoint
-         * bf:hasAuthority
-         * bf:systemNumber
-         * Predicates of which Meetings are objects:
-         * bf:contributor
-         * bf:creator
-         * bf:subject
-         */
+        // Do with the identifier - will be common to other types
+        // convertFastIdentifier();
+        
+        // if the meeting is a subject, and there's a fast identifier, 
+        // create a sameas between the meeting and the fast uri
 
         return super.convert();        
     }
@@ -70,8 +95,28 @@ public class BfMeetingConverter extends BfAuthorityConverter {
                         subject, RDF.type, Ld4lType.CONFERENCE.ontClass());
         }
         
-        qexec.close();
-        
+        qexec.close();        
     }
-
+    
+    
+    @Override
+    protected List<Property> getPropertiesToRetract() {
+        return BfProperty.properties(propertiesToRetract);
+    }
+    
+//    private void convertFastIdentifier() {
+//        
+//        selectPss.setIri("meeting", subject.getURI());
+//        Query query = selectPss.asQuery();
+//        QueryExecution qexec = 
+//                QueryExecutionFactory.create(query, subject.getModel());
+//        ResultSet results = qexec.execSelect();
+//        while (results.hasNext()) {
+//            
+//        }
+//        // get the fast uri  - static identifierConverter method
+//        // if not null, add the same as to the output model
+//        // add same as to fast subject
+//    }
+    
 }
